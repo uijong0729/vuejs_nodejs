53cfb405a901d9bd70b5b0e884292b3b34292e65
uijong0729 <krocha0729@gmail.com>
2021-05-06 22:26:22 +0900

    include cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <!-- 최상위 요소는 1개만 허용 -->
+  <div>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr">
+
+    </app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: function(){
+    return {
+      str: 'Header'
+    }
+  }, 
+  components: {
+    'app-header': AppHeader,
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'Hello'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
